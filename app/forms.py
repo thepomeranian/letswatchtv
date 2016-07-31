40d8699d69ef49e4bb95145289b5d11295dbc008
Frances Liu <thehamster@gmail.com>
2016-07-31 15:52:54 -0700

    Added jQuery, added docstrings and comments for views.py

--- app/forms.py
+++ app/forms.py
@@ -2,13 +2,14 @@ from flask_wtf import Form
 from wtforms import StringField, BooleanField, PasswordField, FileField, HiddenField, DateField, SelectField, TextAreaField, IntegerField, RadioField, FieldList, FormField, SubmitField
 from wtforms.validators import Required, Optional, NumberRange, Email, EqualTo
 
-
+# Login Form class with form validation
 class LoginForm(Form):
   email = StringField('email', validators = [Required()])
   password = PasswordField('password', validators = [Required()])
   remember = BooleanField('remember', default = False)
 
 
+# Sign Up Form class with form validation
 class SignUpForm(Form):
   first_name = StringField('first_name', validators = [Required()])
   last_name = StringField('last_name', validators = [Required()])
--- app/views.py
+++ app/views.py
@@ -9,14 +9,56 @@ import datetime
 
 @app.route('/')
 def home():
+  """Returns homepage of Let's Watch TV
+  """
   return render_template("index.html", title="Home")
+
 
 @app.route('/search-results')
 def search_results():
+  """Returns a result page from search query
+  """
   return render_template("results.html", title="Results")
+
+
+@app.route('/about')
+def about_page():
+  """Returns a page about Let's Watch TV 
+  """
+  return render_template("about/about.html")
+
+
+@app.route('/user_home')
+def user_home():
+  """Returns user's homepage/landing page
+  """
+  return render_template("account/home.html", title="Home")
+
 
 @app.route('/signup', methods = ['GET', 'POST'])
 def signup():
+  """Gets a signup form and submits it upon successful completion. 
+
+  Checks if current_user is authenticated, if they are, then redirect
+    them to the user's homepage
+
+  Creates an instance of SignUpForm from forms.py as form
+
+  Checks if the the entire form is successfully completed and
+    checks if the user has already created an account in database
+
+  Creates an instance of User() and calls the method populate_obj
+    to insert elements in the form into database
+
+  user.created_on and user.updated_on get a timestamp from datetime
+    library
+
+  Adds user to db.session, commit's add, and login's user
+
+  Flash a success message using toastr library
+
+  Redirect to user's homepage
+  """
   if current_user.is_authenticated:
     return redirect(url_for('user_home'))
 
@@ -34,8 +76,25 @@ def signup():
 
   return render_template('account/signup.html', title='Sign Up', form=form)
 
+
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
+  """Checks if the current_user is logged in, if so, redirect to user's
+    homepage. Handles user login, remember me, and check authentication
+
+  Create an instance of LoginForm
+
+  Check if the form is properly filled it upon submission
+    if so, look for the user by using query method to look in the database
+    of users by email address and return the first response
+
+  If the user exists and the user provides the correct password
+    Check if the user asked to remember the user, if so, keep the session
+    longer (from docs)
+  
+    --add remember me checkbox
+
+  """
   # Already logged in; return to index
   if current_user.is_authenticated:
     return redirect(url_for('user_home'))
@@ -46,13 +105,15 @@ def login():
     user = User.query.filter_by(email = form.email.data).first()
     if user is not None and user.valid_password(form.password.data):
       if login_user(user, remember = form.remember.data):
+        # session['remember_me'] = form.remember_me.data
         session.permanent = not form.remember.data
-        #Need to add proper message flashing code to base.html
         user.created_on = datetime.datetime.now()
         user.last_logged_in = datetime.datetime.now()
         db.session.commit()
         flash('Logged in successfully!', category = 'success')
         return redirect(request.args.get('next') or url_for('user_home'))
+    # If a user is blocked or if the password is incorrect, flash
+    # a danger message
       else:
         flash('This username is disabled', 'danger')
     else:
@@ -60,17 +121,22 @@ def login():
 
   return render_template('account/login.html', title='Login', form=form)
 
+
 @app.route('/logout/')
 @login_required
 def logout():
+  """Logs out user, flashes a message upon completion, and redirects to the homepage
+  """
   logout_user()
   flash('Logged out successfully', 'success')
   return redirect(url_for('home'))
 
+
 @lm.user_loader
 def load_user(id):
+  """Returns the user's unique id from the database,
+    used by Flask-Login
+  """
     return User.query.get(int(id))
 
-@app.route('/user_home')
-def user_home():
-  return render_template("account/home.html", title="Home")
+
