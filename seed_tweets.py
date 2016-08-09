ba72d0684e2b36d5ba9f2ae60082a311b709f0a0
Frances Liu <thehamster@gmail.com>
2016-08-08 17:56:55 -0700

    Played around with stream_listener to take multiple arguments

--- seed_tweets.py
+++ seed_tweets.py
@@ -1,5 +1,11 @@
 from app import models, db
+import json
 
-tweets = models.Tweets()
-db.session.add(tvshow)
-db.session.commit()
+raw_data = open('twitter_python.txt', 'r')
+
+data = json.load(raw_data)
+tweet = raw_data["created_at"]
+print tweet
+# tweets = models.Tweets(username, location, created_at, text)
+# db.session.add(tvshow)
+# db.session.commit()
