cdcc001117eff67214c267642ae61a50d634f6f0
Frances Liu <thehamster@gmail.com>
2016-08-17 14:56:00 -0700

    Cleaned up spaces, need to refactor more in the future

--- seed_tvmaze.py
+++ seed_tvmaze.py
@@ -1,7 +1,21 @@
 import requests
 from app import models, db
 
+
+def get_or_create(session, model, **kwargs):
+"""Checks db for same entry, if duplicate entry does not exist, insert a new entry"""
+    instance = session.query(model).filter_by(**kwargs).first()
+    if instance:
+        return instance
+    else:
+        instance = model(**kwargs)
+        session.add(instance)
+        session.commit()
+        return instance
+
+
 def seed():
+"""Seeds db using the request library and TVmaze API"""
   
   for x in xrange(0,81):
     print "Seeding TVmaze page", x
@@ -11,65 +25,64 @@ def seed():
 
     response = r.json() 
 
-    for tvshow in response:                   
-      schedule = tvshow['schedule']
+    for tvshow_obj in response:                   
+      schedule = tvshow_obj['schedule']
       days = schedule['days']
       string_days = schedule_days(days)
-      externals = tvshow['externals']
+      externals = tvshow_obj['externals']
       string_genres = ""
 
-      if tvshow['genres']:
-        string_genres = stringing_genres(tvshow['genres'])
-      else:
-        tvshow['genres'] = None
+      if tvshow_obj['genres']:
+        string_genres = stringing_genres(tvshow_obj['genres'])
 
-      if tvshow['image']:
-        image_urls = tvshow['image']
-        tvshow_photos = models.TVShowPhoto(medium_url=image_urls['medium'],
+      else:
+        tvshow_obj['genres'] = None
+
+      if tvshow_obj['network']:
+        network_info = tvshow_obj['network']
+        country_info = network_info['country']
+        network = get_or_create(db.session, models.Network,
+                               network_name=network_info['name'], 
+                               country=country_info['name'], 
+                               code=country_info['code'], 
+                               timezone=country_info['timezone'])
+
+      tvshow = models.TVShow(tvshow=tvshow_obj['name'], 
+                             type_=tvshow_obj['type'], 
+                             language=tvshow_obj['language'],
+                             genres=string_genres,
+                             status=tvshow_obj['status'],
+                             premiered=tvshow_obj['premiered'],
+                             schedule_time=schedule['time'],
+                             schedule_day=string_days,
+                             network_id=network.id,
+                             summary=tvshow_obj['summary'])
+      add_to_db(tvshow)
+
+      if tvshow_obj['image']:
+        image_urls = tvshow_obj['image']
+        tvshow_photos = models.TVShowPhoto(tvshow_id=tvshow.id,
+                                           medium_url=image_urls['medium'],
                                            original_url=image_urls['original'])
         add_to_db(tvshow_photos)
 
       else:
-        tvshow['image'] = None
-        tvshow_photos = models.TVShowPhoto(medium_url=None,
+        tvshow_obj['image'] = None
+        tvshow_photos = models.TVShowPhoto(tvshow_id=tvshow.id,
+                                           medium_url=None,
                                            original_url=None)
         add_to_db(tvshow_photos)
 
-
-      if tvshow['network']:
-        network_info = tvshow['network']
-        country_info = network_info['country']
-        network = models.Network(network_name=network_info['name'], 
-                               country=country_info['name'], 
-                               code=country_info['code'], 
-                               timezone=country_info['timezone'])
-        add_to_db(network)
-
-      else:
-        network = models.Network(network_name=None, 
-                               country=None, 
-                               code=None, 
-                               timezone=None)
-        add_to_db(network)
-        
-      external = models.External(tvrage=externals['tvrage'], 
+      external = models.External(tvshow_id=tvshow.id,
+                                 tvrage=externals['tvrage'], 
                                  thetvdb=externals['thetvdb'], 
                                  imdb=externals['imdb'])
       add_to_db(external)
-
-      tvshow = models.TVShow(tvshow=tvshow['name'], 
-                             type_=tvshow['type'], 
-                             language=tvshow['language'],
-                             genres=string_genres,
-                             status=tvshow['status'],
-                             premiered=tvshow['premiered'],
-                             schedule_time=schedule['time'],
-                             schedule_day=string_days,
-                             summary=tvshow['summary'])
-      add_to_db(tvshow)
-
+      
 
 def schedule_days(days):
+  """Changes list to printed string"""
+  # Combine with stringing genres if time allows
   length = len(days)
   string_days = ""
   while length > 0:
@@ -83,12 +96,16 @@ def schedule_days(days):
     length -= 1
   return string_days
 
+
 def add_to_db(models):
+  """Add transaction and commit"""
   db.session.add(models)
   db.session.commit()
 
 
 def stringing_genres(genres):
+  """Changes list to printed string"""
+  # Combine with schedule days if time allows
   length = len(genres)
   string_genres = ""
 
