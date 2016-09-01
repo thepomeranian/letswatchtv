4fccee3822654f00790de9c08dfc4a01bd111ef4
Frances Liu <thehamster@gmail.com>
2016-08-31 23:47:20 -0700

    seed file grabs twitter handle and facebook handle

--- seed_guidebox.py
+++ seed_guidebox.py
@@ -3,13 +3,26 @@ from app import models, db
 import requests
 
 def seed():
-  all_tvshows = models.TVShow.query.filter_by(id).all()
-  print tvshow.externals.imdb
-  # for tvshow in all_tvshows:
-    
-  #   r = requests.get("https://api-public.guidebox.com/v1.43/US/%s/search/id/imdb/%s" % secret.GUIDEBOX_KEY, tvshow.externals.imdb )
+  all_tvshows = models.TVShow.query.all()
+  
+  for tvshow in all_tvshows:
+    print tvshow.tvshow
 
-  #   response = r.json()
+    for external in tvshow.externals:
+      r = requests.get("https://api-public.guidebox.com/v1.43/US/%s/search/id/imdb/%s" % (secret.GUIDEBOX_KEY, external.imdb) )
+      response = r.json()
+      guidebox_id = response['id']
 
-  # for tvshow_obj in response:
-  #   pass
+      r2 = requests.get("https://api-public.guidebox.com/v1.43/US/%s/show/%s" % (secret.GUIDEBOX_KEY, guidebox_id))
+      responses2 = r2.json()
+      
+      medias = responses2['channels']
+      media = medias[0]
+      social_media = media['social']
+      facebook = social_media['facebook']
+      facebook_id = facebook['facebook_id']
+      
+      twitter = social_media['twitter']
+      twitter_id = twitter['twitter_id']
+      
+seed()
